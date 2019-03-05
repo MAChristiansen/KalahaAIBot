3b2c18d586033104fb00a3c6515149025501284e
Marcus August Christiansen <mac080497@gmail.com>
2019-03-05 11:24:24 +0100

    gameBoard

--- app/src/main/java/com/snm/dk/kalahaaibot/ActivityMainPage.java
+++ app/src/main/java/com/snm/dk/kalahaaibot/ActivityMainPage.java
@@ -2,12 +2,18 @@ package com.snm.dk.kalahaaibot;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ActivityMainPage extends AppCompatActivity {
+
+    private TextView tvPlayer1Pit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+        //tvPlayer1Pit.findViewById(R.id.);
+
     }
 }
