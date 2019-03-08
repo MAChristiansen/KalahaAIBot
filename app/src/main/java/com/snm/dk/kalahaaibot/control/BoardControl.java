7daa40f44f2897cac96b011b2f2fa92cebff0d96
Steen Hansen <steen_home@hotmail.com>
2019-03-08 17:54:43 +0100

    Lavet alt om, så vi nu kun har 2 arrays.

--- app/src/main/java/com/snm/dk/kalahaaibot/control/BoardControl.java
+++ app/src/main/java/com/snm/dk/kalahaaibot/control/BoardControl.java
@@ -15,76 +15,81 @@ public class BoardControl {
 
     private List<Integer> player1Score;
     private List<Integer> player2Score;
+    private List<Integer> playerScores;
 
     public BoardControl() {
         this.player1Score = new ArrayList<>();
         for (int i = 0; i < this.ROW_LENGTH; i++) { this.player1Score.add(BALLS_PER_AMBO); }
-        this.player1Score.add(0);
 
         this.player2Score = new ArrayList<>();
         for (int i = 0; i < this.ROW_LENGTH; i++) { this.player2Score.add(BALLS_PER_AMBO); }
-        this.player2Score.add(0);
+
+        this.player1Score.addAll(this.player2Score);
+
+        this.playerScores = new ArrayList<>();
+        this.playerScores.add(0);
+        this.playerScores.add(0);
     }
 
     public void updateBoard(List<Button> buttons, List<TextView> textViews) {
-        for (int i = 0; i < buttons.size()/2; i++) {
-            buttons.get(i).setText(this.player2Score.get(i).toString());
-            buttons.get(i+6).setText(this.player1Score.get(i).toString());
+        for (int i = 0; i < buttons.size(); i++) {
+            buttons.get(i).setText(this.player1Score.get(i).toString());
         }
         // Player1
-        textViews.get(0).setText(this.player1Score.get(this.player1Score.size()-1).toString());
+        textViews.get(0).setText(this.playerScores.get(0).toString());
         // Player2
-        textViews.get(1).setText(this.player2Score.get(this.player2Score.size()-1).toString());
+        textViews.get(1).setText(this.playerScores.get(1).toString());
+
+        Log.e("HELLO", this.player1Score.toString());
     }
 
-    public void moveAMBO(int playerPick) {
-        int translatedPick = 0;
-        // Player1
-        if (playerPick >= 5 && playerPick <= 11) {
-            switch (playerPick) {
-                case 6: translatedPick = 0; break;
-                case 7: translatedPick = 1; break;
-                case 8: translatedPick = 2; break;
-                case 9: translatedPick = 3; break;
-                case 10: translatedPick = 4; break;
-                case 11: translatedPick = 5; break;
-            }
-            updateScore(translatedPick, true);
-        }
-
-        // Player2
-        if (playerPick >= 0 && playerPick < 5) {
-            updateScore(playerPick, false);
-        }
-    }
-
-    private void updateScore(int playerPick, boolean player) {
+    public void moveAMBO(int playerPick, boolean iteration, int AMBO, boolean player) {
         int tempAMBO;
+        int iterationInt = 0;
+        tempAMBO = AMBO;
 
-        // Player1
-        if (player) {
+        if (!iteration) {
             tempAMBO = this.player1Score.get(playerPick);
-            for (int i = playerPick; i < this.player1Score.size(); i++) {
-                this.player1Score.set(i, 1+this.player1Score.get(i));
-                if (tempAMBO == 0) {break;}
-                tempAMBO--;
-            }
-
-            if (tempAMBO > 0) {
-                for (int i = this.player2Score.size(); i > 0 ; i--) {
-
-                    tempAMBO--;
-
-                }
-            }
-
+            iterationInt = 1;
             this.player1Score.set(playerPick, 0);
         }
 
-        // Player2
-        if (!player) {
-
+        if (playerPick >= 6 && playerPick <= 13) {
+            if (tempAMBO > 0) {
+                for (int i = playerPick+iterationInt; i < this.player1Score.size(); i++) {
+                    if (tempAMBO == 0) {break;}
+                    this.player1Score.set(i, 1 + this.player1Score.get(i));
+                    tempAMBO--;
+                }
+                // Recursive if anything is left in tempAMBO
+                if (tempAMBO > 0) {
+                    if (player) {
+                        this.playerScores.set(0, 1 + this.playerScores.get(0));
+                        tempAMBO--;
+                    }
+                    moveAMBO(5, true, tempAMBO, player);
+                }
+            }
         }
+
+        if (playerPick >= 0 && playerPick <= 5) {
+            if (tempAMBO > 0) {
+                for (int i = playerPick-iterationInt; i >= 0; i--) {
+                    if (tempAMBO == 0) {break;}
+                    this.player1Score.set(i, 1 + this.player1Score.get(i));
+                    tempAMBO--;
+                }
+
+                if (tempAMBO > 0) {
+                    if (!player) {
+                        this.playerScores.set(1, 1+this.playerScores.get(1));
+                        tempAMBO--;
+                    }
+                    moveAMBO(6, true, tempAMBO, player);
+                }
+            }
+        }
+
     }
 
 }
