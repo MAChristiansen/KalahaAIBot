a147031df2e322a49d03f495e97661878c084ae1
Steen Hansen <steen_home@hotmail.com>
2019-03-08 17:54:46 +0100

    Update GameControl.java

--- app/src/main/java/com/snm/dk/kalahaaibot/control/GameControl.java
+++ app/src/main/java/com/snm/dk/kalahaaibot/control/GameControl.java
@@ -8,17 +8,27 @@ import java.util.List;
 public class GameControl {
 
     private BoardControl boardControl;
+    private boolean player;
 
     public GameControl() {
         boardControl = new BoardControl();
+        this.player = true;
     }
 
     public GameControl takeTurn(int playerPick) {
-        boardControl.moveAMBO(playerPick);
+        if (this.player && playerPick > 5) {
+            boardControl.moveAMBO(playerPick, false, 0, this.player);
+            this.player = false;
+        } else if (!this.player && playerPick < 5) {
+            boardControl.moveAMBO(playerPick, false, 0, this.player);
+            this.player = true;
+        }
+
         return this;
     }
 
     public void updateBoard(List<Button> buttons, List<TextView> textViews) {
+        if (this.player) textViews.get(2).setText("Player 1 turn"); else textViews.get(2).setText("Player 2 turn");
         boardControl.updateBoard(buttons, textViews);
     }
 
