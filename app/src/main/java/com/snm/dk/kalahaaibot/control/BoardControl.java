cd48beac87d6280f010f62b4ca8f92f47a5530b3
Steen Hansen <steen_home@hotmail.com>
2019-03-12 13:51:58 +0100

    Revert "Player 1 kan flytte AMBOs men kun inden for egen felt."
    This reverts commit 03981d560175ebb7d77d1bc8431c40448c38ae81.

--- app/src/main/java/com/snm/dk/kalahaaibot/control/BoardControl.java
+++ app/src/main/java/com/snm/dk/kalahaaibot/control/BoardControl.java
@@ -1,6 +1,5 @@
 package com.snm.dk.kalahaaibot.control;
 
-import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -12,73 +11,77 @@ public class BoardControl {
     private final int ROW_LENGTH = 12;
     private final int BALLS_PER_AMBO = 6;
 
-    private final String TAG = "Test";
-    private List<Integer> AMBOs;
-    private List<Integer> playerPits;
+    private List<Integer> playerAMBO;
+    private List<Integer> playerScores;
 
     public BoardControl() {
-        this.AMBOs = new ArrayList<>();
-        for (int i = 0; i < this.ROW_LENGTH; i++) { this.AMBOs.add(BALLS_PER_AMBO); }
+        this.playerAMBO = new ArrayList<>();
+        for (int i = 0; i < this.ROW_LENGTH; i++) { this.playerAMBO.add(BALLS_PER_AMBO); }
 
-        this.playerPits = new ArrayList<>();
-        this.playerPits.add(0);
-        this.playerPits.add(0);
+        this.playerScores = new ArrayList<>();
+        this.playerScores.add(0);
+        this.playerScores.add(0);
     }
 
     public void updateBoard(List<Button> buttons, List<TextView> textViews) {
         for (int i = 0; i < buttons.size(); i++) {
-            buttons.get(i).setText(this.AMBOs.get(i).toString());
+            buttons.get(i).setText(this.playerAMBO.get(i).toString());
         }
         // Player1
-        textViews.get(0).setText(this.playerPits.get(0).toString());
+        textViews.get(0).setText(this.playerScores.get(0).toString());
         // Player2
-        textViews.get(1).setText(this.playerPits.get(1).toString());
+        textViews.get(1).setText(this.playerScores.get(1).toString());
     }
 
-    public void moveAMBO(int playerPick, int AMBO) {
+    public void moveAMBO(int playerPick, boolean iteration, int AMBO, boolean player) {
+        int tempAMBO;
+        int iterationInt = 0;
+        tempAMBO = AMBO;
 
-        int pickedAMBO = AMBO;
-        int i = 0;
+        if (!iteration) {
+            tempAMBO = this.playerAMBO.get(playerPick);
+            iterationInt = 1;
+            this.playerAMBO.set(playerPick, 0);
+        }
 
-        // Player1
-        if (playerPick > 5) {
-            for (i = playerPick; i < AMBOs.size()-1; i++) {
-                if (i == playerPick) {
-                    pickedAMBO = AMBOs.get(i);
-                    AMBOs.set(i, 0);
+        // Player1 + recursive
+        if (playerPick >= 6 && playerPick <= 13) {
+            if (tempAMBO > 0) {
+                for (int i = playerPick+iterationInt; i < this.playerAMBO.size(); i++) {
+                    if (tempAMBO == 0) {break;}
+                    this.playerAMBO.set(i, 1 + this.playerAMBO.get(i));
+                    tempAMBO--;
                 }
-                AMBOs.set(i+1, AMBOs.get(i+1)+1);
-                pickedAMBO--;
-            }
-
-            if (pickedAMBO > 0) {
-                if (i == 11 && pickedAMBO >= 1) {
-                    playerPits.set(0, 1 + playerPits.get(0));
-                    pickedAMBO--;
-                    moveAMBO(5, pickedAMBO);
+                // Recursive if anything is left in tempAMBO
+                if (tempAMBO > 0) {
+                    if (player) {
+                        this.playerScores.set(0, 1 + this.playerScores.get(0));
+                        tempAMBO--;
+                    }
+                    moveAMBO(5, true, tempAMBO, player);
                 }
             }
         }
 
-        // Player2
-        if (playerPick <= 5) {
+        // Player2 + recursive
+        if (playerPick >= 0 && playerPick <= 5) {
+            if (tempAMBO > 0) {
+                for (int i = playerPick-iterationInt; i >= 0; i--) {
+                    if (tempAMBO == 0) {break;}
+                    this.playerAMBO.set(i, 1 + this.playerAMBO.get(i));
+                    tempAMBO--;
+                }
 
+                if (tempAMBO > 0) {
+                    if (!player) {
+                        this.playerScores.set(1, 1+this.playerScores.get(1));
+                        tempAMBO--;
+                    }
+                    moveAMBO(6, true, tempAMBO, player);
+                }
+            }
         }
 
-    }
-
-    // TODO: Remove Later.
-    public int getCount() {
-        int count = 0;
-
-        for (int i : AMBOs) {
-            count += i;
-        }
-
-        count += playerPits.get(0);
-        count += playerPits.get(1);
-
-        return count;
     }
 
 }
