21e0f49d043355aa54e7a28061e036702156a1c1
Marcus August Christiansen <mac080497@gmail.com>
2019-03-18 21:39:57 +0100

    Update ActivityMainPage.java

--- app/src/main/java/com/snm/dk/kalahaaibot/view/ActivityMainPage.java
+++ app/src/main/java/com/snm/dk/kalahaaibot/view/ActivityMainPage.java
@@ -1,16 +1,19 @@
 package com.snm.dk.kalahaaibot.view;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.snm.dk.kalahaaibot.R;
 import com.snm.dk.kalahaaibot.control.BoardControl;
 import com.snm.dk.kalahaaibot.control.ControlReg;
 import com.snm.dk.kalahaaibot.control.GameControl;
+import com.snm.dk.kalahaaibot.control.VisualControl;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +24,7 @@ public class ActivityMainPage extends AppCompatActivity implements View.OnClickL
 
     private List<Button> buttons;
     private List<TextView> textViews;
+    private FrameLayout layer, fragmentContainer;
 
     private final int[] BUTTON_IDS = {R.id.btnP2_1, R.id.btnP2_2, R.id.btnP2_3, R.id.btnP2_4, R.id.btnP2_5, R.id.btnP2_6,
             R.id.btnP1_1, R.id.btnP1_2, R.id.btnP1_3, R.id.btnP1_4, R.id.btnP1_5, R.id.btnP1_6, };
@@ -31,6 +35,9 @@ public class ActivityMainPage extends AppCompatActivity implements View.OnClickL
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+        layer = findViewById(R.id.layer);
+        fragmentContainer = findViewById(R.id.FragmentContainer);
 
         int k = 0;
         // Building Button Array
