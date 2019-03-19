98b0abe1491f68258f8b303e093c5eb7ff9a604b
Marcus August Christiansen <mac080497@gmail.com>
2019-03-19 13:27:03 +0100

    Create CalculateStates, but not finished

--- app/src/main/java/com/snm/dk/kalahaaibot/control/AIControl.java
+++ app/src/main/java/com/snm/dk/kalahaaibot/control/AIControl.java
@@ -2,7 +2,10 @@ package com.snm.dk.kalahaaibot.control;
 
 import android.util.Log;
 
+import com.snm.dk.kalahaaibot.model.Board;
 import com.snm.dk.kalahaaibot.model.Node;
+import com.snm.dk.kalahaaibot.model.State;
+import com.snm.dk.kalahaaibot.model.Tree;
 
 public class AIControl {
 
@@ -22,6 +25,22 @@ public class AIControl {
             //The node is a Leaf!
             Log.i(TAG, "visitNode = LEAF! ");
         }
+    }
+
+    public void calculateStates(Board board) {
+
+        State rootState = new State(board);
+        Node root = new Node(rootState);
+        Tree tree = new Tree(root);
+
+        //herfra skal vi findes alle børn til root. Og derefter børnene til børnene osv.
+
+        //visitNode(root);
+        for (int i = 0; i< depth; i++) {
+            //laver sit shiiit!
+        }
+
+
     }
 
 }
