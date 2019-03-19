2f1b442ad5969b4273d20794aa026d1c4bc12bc9
Marcus August Christiansen <mac080497@gmail.com>
2019-03-19 11:26:57 +0100

    Now we can visit all nodes in the tree

--- app/src/main/java/com/snm/dk/kalahaaibot/control/AIControl.java
+++ app/src/main/java/com/snm/dk/kalahaaibot/control/AIControl.java
@@ -1,6 +1,27 @@
 package com.snm.dk.kalahaaibot.control;
 
+import android.util.Log;
+
+import com.snm.dk.kalahaaibot.model.Node;
+
 public class AIControl {
 
+    private final String TAG = "AIControl";
     private final Integer depth = 4;
+
+    public void visitNode(Node node) {
+
+        Log.i(TAG, "visitNode: Utility = " + node.getState().getUtility());
+
+        if (!(node.getChildren().isEmpty())) {
+            for (Node n : node.getChildren()) {
+                visitNode(n);
+            }
+        }
+        else {
+            //The node is a Leaf!
+            Log.i(TAG, "visitNode = LEAF! ");
+        }
+    }
+
 }
