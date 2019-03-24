42b1135876f68ec4e95b7818d3582dad7c09c0e4
Steen Hansen <steen_home@hotmail.com>
2019-03-24 11:32:20 +0100

    Ombygget board control til ikke længere at indholde sit eget board.

--- app/src/main/java/com/snm/dk/kalahaaibot/control/BoardControl.java
+++ app/src/main/java/com/snm/dk/kalahaaibot/control/BoardControl.java
@@ -12,83 +12,84 @@ import java.util.List;
 public class BoardControl {
 
     public static final int ROW_LENGTH = 12;
-    private final int BALLS_PER_AMBO = 6;
+    public static final int BALLS_PER_AMBO = 6;
 
     private final String TAG = "BoardControl";
-    private Board currentBoard;
 
     public BoardControl() {
-        // Init currentBoard
-        currentBoard = new Board();
-        for (int i = 0; i < this.ROW_LENGTH; i++) {
-            this.currentBoard.getAmboScores().add(BALLS_PER_AMBO);
-        }
 
-        this.currentBoard.getPitScores().add(0);
-        this.currentBoard.getPitScores().add(0);
     }
 
+    public Board initBoard(Board board) {
+        for (int i = 0; i < ROW_LENGTH; i++) {
+            board.getAmboScores().add(BALLS_PER_AMBO);
+        }
 
+        board.getPitScores().add(0);
+        board.getPitScores().add(0);
 
-    public void updateBoard(List<Button> buttons, List<TextView> textViews) {
+        return new Board(board.getAmboScores(), board.getPitScores());
+    }
+
+    public void updateBoard(List<Button> buttons, List<TextView> textViews, Board board) {
         for (int i = 0; i < buttons.size(); i++) {
-            buttons.get(i).setText(this.currentBoard.getAmboScores().get(i).toString());
+            buttons.get(i).setText(board.getAmboScores().get(i).toString());
         }
         // Player1
-        textViews.get(0).setText(this.currentBoard.getPitScores().get(0).toString());
+        textViews.get(0).setText(board.getPitScores().get(0).toString());
         // Player2
-        textViews.get(1).setText(this.currentBoard.getPitScores().get(1).toString());
+        textViews.get(1).setText(board.getPitScores().get(1).toString());
     }
 
-    public List<String> updateAmbos() {
+    public List<String> updateAmbos(Board board) {
 
         List<String> amboScores = new ArrayList<>();
 
         for (int i = 0; i < ROW_LENGTH; i++) {
-            amboScores.set(i, this.currentBoard.getAmboScores().get(i).toString());
+            amboScores.set(i, board.getAmboScores().get(i).toString());
         }
 
         return amboScores;
     }
 
-    public List<String> updatePits() {
+    public List<String> updatePits(Board board) {
 
         List<String> pitScores = new ArrayList<>();
 
-        pitScores.set(0, this.currentBoard.getPitScores().get(0).toString());
-        pitScores.set(1, this.currentBoard.getPitScores().get(1).toString());
+        pitScores.set(0, board.getPitScores().get(0).toString());
+        pitScores.set(1, board.getPitScores().get(1).toString());
 
         return pitScores;
     }
 
-    public boolean moveAMBO(int playerPick, boolean iteration, int AMBO, boolean player) {
+    public boolean moveAMBO(int playerPick, boolean iteration, int AMBO, boolean player, Board board) {
         int i = 0;
         int tempAMBO;
         int iterationInt = 0;
         tempAMBO = AMBO;
 
         if (!iteration) {
-            tempAMBO = this.currentBoard.getAmboScores().get(playerPick);
+            tempAMBO = board.getAmboScores().get(playerPick);
             iterationInt = 1;
-            this.currentBoard.getAmboScores().set(playerPick, 0);
+            board.getAmboScores().set(playerPick, 0);
         }
 
         // Player1 + recursive
         if (playerPick >= 6 && playerPick <= 13) {
             if (tempAMBO > 0) {
-                for (i = playerPick+iterationInt; i < this.currentBoard.getAmboScores().size(); i++) {
+                for (i = playerPick+iterationInt; i < board.getAmboScores().size(); i++) {
                     if (tempAMBO == 0) {break;}
-                    this.currentBoard.getAmboScores().set(i, 1 + this.currentBoard.getAmboScores().get(i));
+                    board.getAmboScores().set(i, 1 + board.getAmboScores().get(i));
                     tempAMBO--;
                 }
                 i--;
 
                 Log.i(TAG, "AMBO: " + tempAMBO + " i: " + i);
                 // If player1 put the last ball in a empty ambo, both the remaining ball and the opponents straight ambo.
-                if (player && tempAMBO == 0 && currentBoard.getAmboScores().get(i) == 1) {
-                    currentBoard.getPitScores().set(0, currentBoard.getAmboScores().get(i) + currentBoard.getPitScores().get(0) + currentBoard.getAmboScores().get(i-6));
-                    currentBoard.getAmboScores().set(i, 0);
-                    currentBoard.getAmboScores().set(i-6, 0);
+                if (player && tempAMBO == 0 && board.getAmboScores().get(i) == 1) {
+                    board.getPitScores().set(0, board.getAmboScores().get(i) + board.getPitScores().get(0) + board.getAmboScores().get(i-6));
+                    board.getAmboScores().set(i, 0);
+                    board.getAmboScores().set(i-6, 0);
                     return false;
                 }
 
@@ -97,13 +98,13 @@ public class BoardControl {
                     if (player) {
                         // If player1 put the last ball in his pit -> extra turn
                         if (tempAMBO == 1) {
-                            this.currentBoard.getPitScores().set(0, 1 + this.currentBoard.getPitScores().get(0));
+                            board.getPitScores().set(0, 1 + board.getPitScores().get(0));
                             return true;
                         }
-                        this.currentBoard.getPitScores().set(0, 1 + this.currentBoard.getPitScores().get(0));
+                        board.getPitScores().set(0, 1 + board.getPitScores().get(0));
                         tempAMBO--;
                     }
-                    moveAMBO(5, true, tempAMBO, player);
+                    moveAMBO(5, true, tempAMBO, player, board);
                 }
             }
         }
@@ -113,17 +114,17 @@ public class BoardControl {
             if (tempAMBO > 0) {
                 for (i = playerPick-iterationInt; i >= 0; i--) {
                     if (tempAMBO == 0) {break;}
-                    this.currentBoard.getAmboScores().set(i, 1 + this.currentBoard.getAmboScores().get(i));
+                    board.getAmboScores().set(i, 1 + board.getAmboScores().get(i));
                     tempAMBO--;
                 }
                 i++;
 
                 Log.i(TAG, "AMBO: " + tempAMBO + " i: " + i);
                 // If player2 put the last ball in a empty ambo, both the remaining ball and the opponents straight ambo.
-                if (!player && tempAMBO == 0 && currentBoard.getAmboScores().get(i) == 1) {
-                    currentBoard.getPitScores().set(1, currentBoard.getAmboScores().get(i) + currentBoard.getPitScores().get(1) + currentBoard.getAmboScores().get(i+6));
-                    currentBoard.getAmboScores().set(i, 0);
-                    currentBoard.getAmboScores().set(i+6, 0);
+                if (!player && tempAMBO == 0 && board.getAmboScores().get(i) == 1) {
+                    board.getPitScores().set(1, board.getAmboScores().get(i) + board.getPitScores().get(1) + board.getAmboScores().get(i+6));
+                    board.getAmboScores().set(i, 0);
+                    board.getAmboScores().set(i+6, 0);
                     return true;
                 }
 
@@ -132,13 +133,13 @@ public class BoardControl {
                     if (!player) {
                         // If player1 put the last ball in his pit -> extra turn
                         if (tempAMBO == 1) {
-                            this.currentBoard.getPitScores().set(1, 1+this.currentBoard.getPitScores().get(1));
+                            board.getPitScores().set(1, 1 + board.getPitScores().get(1));
                             return false;
                         }
-                        this.currentBoard.getPitScores().set(1, 1+this.currentBoard.getPitScores().get(1));
+                        board.getPitScores().set(1, 1 + board.getPitScores().get(1));
                         tempAMBO--;
                     }
-                    moveAMBO(6, true, tempAMBO, player);
+                    moveAMBO(6, true, tempAMBO, player, board);
                 }
             }
         }
@@ -147,21 +148,16 @@ public class BoardControl {
         if (player) {return  false;} else {return true;}
     }
 
-    public int getCount() {
+    public int getCount(Board board) {
         int count = 0;
-        Log.i(TAG, currentBoard.getAmboScores().toString());
-        for (int i : currentBoard.getAmboScores()) {
+        Log.i(TAG, board.getAmboScores().toString());
+        for (int i : board.getAmboScores()) {
             count += i;
 
         }
-            count += currentBoard.getPitScores().get(0);
-            count += currentBoard.getPitScores().get(1);
+            count += board.getPitScores().get(0);
+            count += board.getPitScores().get(1);
             return count;
     }
 
-    public Board getCurrentBoard() {
-        return currentBoard;
-    }
-
-    public void setCurrentBoard(Board board) {this.currentBoard = board;}
 }
